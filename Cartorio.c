4f3344cd0dc2de163c925a6e2e47effcb0eab9fb
rafaelpc81 <145807811+rafaelpc81@users.noreply.github.com>
2023-09-22 18:36:23 -0300

    Add files via upload

--- Cartorio.c
+++ Cartorio.c
@@ -126,6 +126,7 @@ int main ()
         printf("\t1 - Registrar nomes\n");
         printf("\t2 - Consultar nomes\n");
         printf("\t3 - Deletar nomes\n\n");
+        printf(" - Sair do sistema\n\n");
         printf("Opção: "); //Fim do menu
     
         scanf("%d", &opcao); //Armazenando a escolha do usuário
@@ -146,6 +147,11 @@ int main ()
     	    case 3:
     	    deletar(); //Chamada de funcções, deletar
     	    break;
+    	    
+    	    case 4:
+			printf("Obrigado por usar o nosso sistema!\n");
+			return 0;
+			break;
     	    
     	    
     	    default:
